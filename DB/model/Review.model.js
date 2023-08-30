acf697fbc4600bc2c55d2e227a05f157ebccfadb
OsamaNuserat <osamanuserat3@gmail.com>
2023-08-30 22:36:01 +0300

    comment good to go

--- DB/model/Review.model.js
+++ DB/model/Review.model.js
@@ -1,35 +1,31 @@
-import mongoose, {Schema,Types,model} from 'mongoose';
-const reviewSchema = new Schema ({
-  comment : {
-    type:String,
-    required:true,
-    
-  },
-  categoryId: { 
-    type: Types.ObjectId,
-    ref: "Category",
-    required: true,
+import mongoose, { Schema, Types, model } from "mongoose";
+const reviewSchema = new Schema(
+  {
+    comment: {
+      type: String,
+      required: true,
+    },
+    categoryId: {
+      type: Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
 
   },
-  createdBy : {
-    type: Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  rating : {
-    type:Number,
-    required:true,
-    min:1,
-    max:5
-  },
-  orderId : {
-    type: Types.ObjectId,
-    ref: "Order",
-    required: true,
+  {
+    timestamps: true,
   }
-},
-{
-    timestamps:true
-})
-const reviewModel = mongoose.models.Review ||  model('Review', reviewSchema); 
+);
+const reviewModel = mongoose.models.Review || model("Review", reviewSchema);
 export default reviewModel;
--- src/Modules/Review/Controller/Review.Controller.js
+++ src/Modules/Review/Controller/Review.Controller.js
@@ -5,20 +5,24 @@ import { asyncHandler } from "../../../Services/errorHandling.js";
 export const createReview = asyncHandler(async (req, res,next) => { 
     const {categoryId} = req.params;
     const {comment,rating} = req.body;
-    const order = await orderModel.findOne({userId:req.user._id,Products:{$elemMatch:{categoryId}}});
-    if(!order) {
-        return next(new Error("product not found"),{cause:400});
-    }
-    const checkReview = await reviewModel.findOne({orderId:order._id,categoryId});
-    if(checkReview) {
-        return next(new Error("review already exist"),{cause:400});
-    }
+
+    // const order = await orderModel.findOne({
+    //     userId:req.user._id,
+    //     "Products.categoryId":categoryId
+    // });
+    // if(!order) {
+    //     return next(new Error("product not found"),{cause:400});
+    // }
+    // const checkReview = await reviewModel.findOne({orderId:order._id,categoryId});
+    // if(checkReview) {
+    //     return next(new Error("review already exist"),{cause:400});
+    // }
+
     const review = await reviewModel.create({
         comment,
         rating,
         categoryId,
         createdBy:req.user._id,
-        orderId:order._id
     })
     return res.status(201).json({message:"success",review});
 })
